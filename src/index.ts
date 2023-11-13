a6fe75ad5abf56993cc55d2f177c525d49b02057
Thomas Ramé <thomas.rame@outlook.com>
2023-11-13 19:19:08 +0100

    feat: download and unzip source code

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import AdmZip from 'adm-zip';
 import fsSync from 'fs';
 import fs from 'fs/promises';
 import path from 'path';
@@ -60,6 +61,17 @@ export async function getHTML(url: string): Promise<string> {
   return html;
 }
 
+export async function downloadFile(url: string, destination: string): Promise<void> {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Failed to download file: ${response.status} ${response.statusText}`);
+  }
+
+  const content = await response.arrayBuffer();
+  await fs.writeFile(destination, new Uint8Array(content));
+}
+
 export async function main() {
   // Cannot be imported directly due because it needs `"type": "module"` but Playwright cannot work with it
   const { $ } = await import('execa');
@@ -68,8 +80,12 @@ export async function main() {
 
   console.log('starting gathering information');
 
+  // Define unique ID for this project
+  // TODO: should a merged of production website URL and main repository (don't know how to calculate it right now) + be compatible with filename format
+  const projectId = initiativeToProcess.name;
+
   // Get content
-  const projectDirectory = path.resolve(__dirname, '../data/');
+  const projectDirectory = path.resolve(__dirname, '../data/', projectId);
   const htmlPath = path.resolve(projectDirectory, 'website.html');
 
   if (!fsSync.existsSync(htmlPath)) {
@@ -80,12 +96,32 @@ export async function main() {
   }
 
   // Reduce content by using `pandoc` to save tokens on GPT
-  const noImgAndSvgFilterPath = path.resolve(__dirname, '../src/pandoc/no-img-and-svg.lua');
-  const extractMetaDescriptionFilterPath = path.resolve(__dirname, '../src/pandoc/extract-meta-description.lua');
-
   const markdownPath = path.resolve(projectDirectory, 'website.md');
 
-  await $`pandoc ${htmlPath} --lua-filter ${noImgAndSvgFilterPath} --lua-filter ${extractMetaDescriptionFilterPath} -t gfm-raw_html -o ${markdownPath}`;
+  if (!fsSync.existsSync(markdownPath)) {
+    const noImgAndSvgFilterPath = path.resolve(__dirname, '../src/pandoc/no-img-and-svg.lua');
+    const extractMetaDescriptionFilterPath = path.resolve(__dirname, '../src/pandoc/extract-meta-description.lua');
+
+    await $`pandoc ${htmlPath} --lua-filter ${noImgAndSvgFilterPath} --lua-filter ${extractMetaDescriptionFilterPath} -t gfm-raw_html -o ${markdownPath}`;
+  }
+
+  // Get the source code
+  // TODO: only for Github for now, to adjust (using GitHub API domain helped not guessing the default branch)
+  const repositoryUrl = new URL('https://github.com/inclusion-numerique/mediature');
+  const sourceCodeZipUrl = `https://api.github.com/repos${repositoryUrl.pathname}/zipball`;
+  const zipPath = path.resolve(projectDirectory, 'code.zip');
+
+  if (!fsSync.existsSync(zipPath)) {
+    const codeFolderPath = path.resolve(projectDirectory, 'code');
+
+    await downloadFile(sourceCodeZipUrl, zipPath);
+
+    const zip = new AdmZip(zipPath);
+    zip.extractAllTo(codeFolderPath, true);
+    // TODO: use async but requires intermediate promise (whereas they are "Promise" variants for other methods...)
+    // zip.extractAllToAsync(codeFolderPath, true, () => {});
+    // Note: it's unzipped in its own top folder
+  }
 }
 
 main();

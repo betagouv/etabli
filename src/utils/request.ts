600be9bfe92370e43aaff901b29df2ed9d25cda0
Thomas Ramé <thomas.rame@outlook.com>
2024-02-23 12:28:17 +0100

    chore: ignore from alerting a few more reachability errors

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -7,21 +7,33 @@ export function handleReachabilityError(error: Error) {
   if (
     ![
       // The server is unreachable, since the route may be broken temporarily we skip the domain to be reprocessed next time
+      'EAI_AGAIN',
       'ECONNREFUSED',
       'ECONNRESET',
+      'EHOSTUNREACH',
       'ENETUNREACH',
       'ENOTFOUND',
-      'UND_ERR_SOCKET',
+      'ETIMEDOUT',
       'UND_ERR_CONNECT_TIMEOUT',
+      'UND_ERR_HEADERS_TIMEOUT',
+      'UND_ERR_SOCKET',
       // Some websites return wrongly formatted headers (like https://mesads.beta.gouv.fr/robots.txt due to the provider Clever Cloud)
       // Which makes `fetch()` failing with `HPE_INVALID_HEADER_TOKEN ... Invalid header value char`.
       'HPE_INVALID_HEADER_TOKEN',
-      // Hostname/IP does not match certificate's altnames (which makes the certificate invalid)
-      'ERR_TLS_CERT_ALTNAME_INVALID',
+      // Invalid certificate
+      'ERR_SSL_TLSV1_ALERT_INTERNAL_ERROR',
+      'ERR_TLS_CERT_ALTNAME_INVALID', // Hostname/IP does not match certificate's altnames (which makes the certificate invalid)
       'DEPTH_ZERO_SELF_SIGNED_CERT',
       'UNABLE_TO_VERIFY_LEAF_SIGNATURE',
       'CERT_HAS_EXPIRED',
-    ].includes((error.cause as any)?.code || '')
+      'SELF_SIGNED_CERT_IN_CHAIN',
+    ].includes((error.cause as any)?.code || '') &&
+    !(
+      error instanceof TypeError &&
+      [
+        'redirect count exceeded', // Having a 301 redirection on itself exemple example
+      ].includes(error.message)
+    )
   ) {
     // Since we do not throw error, we log them for record but we also notify Sentry so we can make the list above updated with appropriate codes
     console.error(error);

e0d4c388448a710d226316106421bf44c2675be9
Thomas Ramé <thomas.rame@outlook.com>
2024-02-25 23:20:12 +0100

    chore(cli): listen for errors to shut the program properly while logging the necessary

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,4 +1,7 @@
 import { program } from '@etabli/src/cli/program';
+import { gracefulExit } from '@etabli/src/server/system';
 
 // This would break imports from Next.js so isolating it to be run only by CLI
-program.parse();
+program.parseAsync().catch((error) => {
+  gracefulExit(error);
+});

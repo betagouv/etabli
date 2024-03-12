d69be9a068576b3149cc77d48c56ce5d0c869f21
Thomas Ramé <thomas.rame@outlook.com>
2024-03-12 15:58:42 +0100

    chore: do not throw an error when generating the initiative page title if not found

--- src/app/(public)/initiative/[initiativeId]/page.tsx
+++ src/app/(public)/initiative/[initiativeId]/page.tsx
@@ -1,18 +1,29 @@
 import { Metadata } from 'next';
 
 import { InitiativePage, InitiativePageProps } from '@etabli/src/app/(public)/initiative/[initiativeId]/InitiativePage';
+import { GetInitiativeSchema } from '@etabli/src/models/actions/initiative';
 import { prisma } from '@etabli/src/prisma';
 import { formatPageTitle } from '@etabli/src/utils/page';
 
 export async function generateMetadata(props: InitiativePageProps): Promise<Metadata> {
-  const initiative = await prisma.initiative.findUnique({
-    where: {
-      id: props.params.initiativeId,
-    },
-  });
+  let initiativeName: string | null = null;
+
+  try {
+    const initiativeId = GetInitiativeSchema.shape.id.parse(props.params.initiativeId);
+
+    const initiative = await prisma.initiative.findUniqueOrThrow({
+      where: {
+        id: initiativeId,
+      },
+    });
+
+    initiativeName = initiative.name;
+  } catch (error) {
+    // Probably the ID passed is not a valid UUID or existing, we silent the error since it does not change the logic
+  }
 
   return {
-    title: formatPageTitle(!!initiative ? `${initiative.name} - Initiative` : `Initiative`),
+    title: formatPageTitle(!!initiativeName ? `${initiativeName} - Initiative` : `Initiative`),
   };
 }
 

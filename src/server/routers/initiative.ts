173723a4c88edd0b50b17b67fb64f34f5bc0d0ce
Thomas Ramé <thomas.rame@outlook.com>
2024-03-12 20:04:39 +0100

    chore: sort initiatives by default so the pagination as a minimum of sense

--- src/server/routers/initiative.ts
+++ src/server/routers/initiative.ts
@@ -86,6 +86,9 @@ export const initiativeRouter = router({
             },
           },
         },
+        orderBy: {
+          name: 'asc',
+        },
         skip: (input.page - 1) * input.pageSize,
         take: input.pageSize,
       }),
